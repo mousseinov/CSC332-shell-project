909421c942d6fbdbd7d1744848f725765e191c6b
Krystal Leong <kleong000@citymail.cuny.edu>
2019-04-30 17:36:01 -0400

    updated cd

--- src/cd.c
+++ src/cd.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-
-int main(int argc, char* argv[]) {
-
-
-	printf("This is cd.c\n");
-
-	return 1;
+int main(int argc, char* argv[]){
+	char cwd[100];
+	if (getcwd(cwd, 100) == NULL){
+		perror("chdir() error");
+	}
+	else {
+		int cd = chdir("..");
+		if (cd != 0){
+			perror("cd failed: cannot go up any further.\n");
+		}
+		else {
+			argv[1] = getcwd(cwd, 100);
+			printf("%s >>>\n", argv[1]);
+		}
+	}
+	return 0;
 }
