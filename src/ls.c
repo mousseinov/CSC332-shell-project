4857ebdead05a3b54cd4c2992526807666b68a01
angelica-hernandez <42852941+angelica-hernandez@users.noreply.github.com>
2019-04-30 17:14:59 -0400

    Update ls.c

--- src/ls.c
+++ src/ls.c
@@ -1,15 +1,18 @@
-#include <stdio.h>
-
-
-int main(int argc, char* argv[]) {
-
-
-        printf("This is ls.c\n");
-
-        return 1;
-}
-
-
-
-
-
+#include <stdio.h> 
+#include <dirent.h> 
+  
+int main(void) { 
+    struct dirent *director_entry;  // Pointer for directory entry 
+    DIR *directory = opendir("."); // opendir() returns a pointer of DIR type.  
+    if (directory == NULL) { // opendir returns NULL if couldn't open directory  
+        printf("Unable to execute ls command" ); 
+        return 0; 
+    } 
+	//readdir returns pointer to a dirent structure of the next directory entry
+	//overwrites memory of previous readdir() call
+    while ((director_entry = readdir(directory)) != NULL) {
+            printf("%s\n", director_entry->d_name);
+        }
+    closedir(directory);     
+    return 0; 
+} 
